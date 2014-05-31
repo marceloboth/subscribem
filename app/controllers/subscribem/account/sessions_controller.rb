47ceb453d844db158fd05db3f4f94811b95bb31c
Marcelo J. Both <marcelo.both@gmail.com>
2014-05-30 22:56:42 -0300

    Invalid usernames or password fails

--- app/controllers/subscribem/account/sessions_controller.rb
+++ app/controllers/subscribem/account/sessions_controller.rb
@@ -5,5 +5,16 @@ module Subscribem
     def new
       @user = User.new
     end
+
+    def create
+      if env["warden"].authenticate(:scope => :user)
+        flash[:success] = "You are now signed in."
+        redirect_to root_path
+      else
+        @user = User.new
+        flash[:error] = "Invalid email or password."
+        render :action => "new"
+      end
+    end
   end
 end
--- spec/features/users/sign_in_spec.rb
+++ spec/features/users/sign_in_spec.rb
@@ -5,6 +5,7 @@ feature "User sign in" do
   let!(:account) { FactoryGirl.create(:account) }
   let(:sign_in_url) { "http://#{account.subdomain}.example.com/sign_in" }
   let(:root_url) { "http://#{account.subdomain}.example.com/" }
+
   within_account_subdomain do
     scenario "signs in as an account owner successfully" do
       visit root_url
@@ -15,5 +16,28 @@ feature "User sign in" do
       page.should have_content("You are now signed in.")
       page.current_url.should == root_url
     end
+
+    scenario "attempts sign in with an invalid password and fails" do
+      visit subscribem.root_url(:subdomain => account.subdomain)
+      page.current_url.should == sign_in_url
+      page.should have_content("Please sign in.")
+      fill_in "Email", :with => account.owner.email
+      fill_in "Password", :with => "drowssap"
+      click_button "Sign in"
+      page.should have_content("Invalid email or password.")
+      page.current_url.should == sign_in_url
+    end
+
+    scenario "attempts sign in with an invalid email address and fails" do
+      visit subscribem.root_url(:subdomain => account.subdomain)
+      page.current_url.should == sign_in_url
+      page.should have_content("Please sign in.")
+      fill_in "Email", :with => "foo@example.com"
+      fill_in "Password", :with => "password"
+      click_button "Sign in"
+      page.should have_content("Invalid email or password.")
+      page.current_url.should == sign_in_url
+    end
+
   end
 end
