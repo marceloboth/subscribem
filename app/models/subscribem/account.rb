cbdb2f38b11e64582474fdcadfdab980477bd3b0
Marcelo J. Both <marcelo.both@gmail.com>
2014-06-11 22:53:48 -0300

    Finished authentication for accounts

--- app/models/subscribem/account.rb
+++ app/models/subscribem/account.rb
@@ -7,6 +7,9 @@ module Subscribem
     belongs_to :owner, :class_name => "Subscribem::User"
     accepts_nested_attributes_for :owner
 
+    has_many :members, :class_name => "Subscribem::Member"
+    has_many :users, :through => :members
+
     validates :subdomain, :presence => true, :uniqueness => true
 
     EXCLUDED_SUBDOMAINS = %w(admin)
@@ -14,6 +17,14 @@ module Subscribem
       :message => "is not allowed. Please choose another subdomain."
     validates_format_of :subdomain, :with =>  /\A[\w\-]+\Z/i,
       :message => "is not allowed. Please choose another subdomain."
+
+    def self.create_with_owner(params={})
+      account = new(params)
+      if account.save
+        account.users << account.owner
+      end
+      account
+    end
 
   end
 end
